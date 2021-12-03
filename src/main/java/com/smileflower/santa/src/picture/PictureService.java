f09d7ba9927915db3701bfe2b34983e60a4b79cc
kimkyuuuuri <rnflrnfl0633@naver.com>
2021-12-04 01:09:55 +0900

    "랭크 오류 수정"

--- src/main/java/com/smileflower/santa/src/picture/PictureService.java
+++ src/main/java/com/smileflower/santa/src/picture/PictureService.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
-import static com.smileflower.santa.config.BaseResponseStatus.ALREADY_LOGOUT;
+
 
 
 @Service
---
+++ src/main/java/com/smileflower/santa/src/picture/model/PostPictureSaveRes.java
@@ -0,0 +1,13 @@
+package com.smileflower.santa.src.picture.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PostPictureSaveRes {
+    private int saveIdx;
+    private String result;
+}
