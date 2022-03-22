5f0333ca46e99c4101748f336cc3c4f154290258
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-03-23 01:18:31 +0900

    "애플 로그인 수정"

--- src/main/java/com/smileflower/santa/src/social_login/Social_loginController.java
+++ src/main/java/com/smileflower/santa/src/social_login/Social_loginController.java
@@ -128,7 +128,7 @@ public class Social_loginController {
     public BaseResponse<AppleLoginRes> appleLogin(@RequestBody ApplePostUserReq applePostUserReq) {
         System.out.println(applePostUserReq.getUserIdentifier());
         System.out.println(applePostUserReq.getAuthorizationCode());
-        System.out.println("testinㅏg");
+        System.out.println("testiㅇnㅏg");
 
         try{
            //if(socialloginProvider.checkAppleId(applePostUserReq.getUserIdentifier()) != 1) {
